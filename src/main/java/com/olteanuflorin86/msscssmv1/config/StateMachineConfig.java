293443035bdba8f69fed0e14dd9f633ebbea3eaa
Florin Olteanu <olteanuflorin86@gmail.com>
2023-06-13 09:46:14 +0300

    Adding state machine config for states

---
+++ src/main/java/com/olteanuflorin86/msscssmv1/config/StateMachineConfig.java
@@ -0,0 +1,33 @@
+package com.olteanuflorin86.msscssmv1.config;
+
+import java.util.EnumSet;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+
+import com.olteanuflorin86.msscssmv1.domain.PaymentEvent;
+import com.olteanuflorin86.msscssmv1.domain.PaymentState;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@EnableStateMachineFactory
+@Configuration
+public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent> {
+
+	@Override
+	public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) throws Exception {
+		states.withStates()
+			.initial(PaymentState.NEW)
+			.states(EnumSet.allOf(PaymentState.class))
+			.end(PaymentState.AUTH)
+			.end(PaymentState.PRE_AUTH_ERROR)
+			.end(PaymentState.PRE_AUTH);
+	}
+}
+
+
+
+
--- src/main/java/com/olteanuflorin86/msscssmv1/repository/PaymentRepository.java
+++ src/main/java/com/olteanuflorin86/msscssmv1/repository/PaymentRepository.java
@@ -1,9 +1,11 @@
 package com.olteanuflorin86.msscssmv1.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.olteanuflorin86.msscssmv1.domain.Payment;
 
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
 }
