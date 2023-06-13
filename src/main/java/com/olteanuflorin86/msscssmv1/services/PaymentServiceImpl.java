14ad53b561e91700e52239539cbb6e2dbb4b4a2e
Florin Olteanu <olteanuflorin86@gmail.com>
2023-06-13 13:22:19 +0300

    Adding init sm from db

--- src/main/java/com/olteanuflorin86/msscssmv1/services/PaymentServiceImpl.java
+++ src/main/java/com/olteanuflorin86/msscssmv1/services/PaymentServiceImpl.java
@@ -2,6 +2,7 @@ package com.olteanuflorin86.msscssmv1.services;
 
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
 
 import com.olteanuflorin86.msscssmv1.domain.Payment;
@@ -25,17 +26,37 @@ public class PaymentServiceImpl implements PaymentService {
 
 	@Override
 	public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
+		StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
 		return null;
 	}
 
 	@Override
 	public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
+		StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
 		return null;
 	}
 
 	@Override
 	public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
+		StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
 		return null;
 	}
+	
+	private StateMachine<PaymentState, PaymentEvent> build(Long paymentId){
+        Payment payment = paymentRepository.getOne(paymentId);
+
+        StateMachine<PaymentState, PaymentEvent> sm = stateMachineFactory.getStateMachine(Long.toString(payment.getId()));
+
+        sm.stop();
+
+        sm.getStateMachineAccessor()
+                .doWithAllRegions(sma -> {
+                    sma.resetStateMachine(new DefaultStateMachineContext<>(payment.getState(), null, null, null));
+                });
+
+        sm.start();
+
+        return sm;
+    }
 
 }
