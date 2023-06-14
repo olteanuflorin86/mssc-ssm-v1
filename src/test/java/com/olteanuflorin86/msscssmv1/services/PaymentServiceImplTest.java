777c9f8ad42e0ac41631b5d59e5165b250e80fdd
Florin Olteanu <olteanuflorin86@gmail.com>
2023-06-14 14:16:38 +0300

    Completing assn

--- src/test/java/com/olteanuflorin86/msscssmv1/services/PaymentServiceImplTest.java
+++ src/test/java/com/olteanuflorin86/msscssmv1/services/PaymentServiceImplTest.java
@@ -3,6 +3,7 @@ package com.olteanuflorin86.msscssmv1.services;
 import java.math.BigDecimal;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -50,5 +51,23 @@ public class PaymentServiceImplTest {
 
         System.out.println(preAuthedPayment);
 	}
+	
+	@Transactional
+    @RepeatedTest(10)
+    void testAuth() {
+        Payment savedPayment = paymentService.newPayment(payment);
+
+        StateMachine<PaymentState, PaymentEvent> preAuthSM = paymentService.preAuth(savedPayment.getId());
+
+        if (preAuthSM.getState().getId() == PaymentState.PRE_AUTH) {
+            System.out.println("Payment is Pre Authorized");
+
+            StateMachine<PaymentState, PaymentEvent> authSM = paymentService.authorizePayment(savedPayment.getId());
+
+            System.out.println("Result of Auth: " + authSM.getState().getId());
+        } else {
+            System.out.println("Payment failed pre-auth...");
+        }
+    }
 
 }
